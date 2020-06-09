0ce22cf21a069f08eecfba3639bb93d558dda8d4
Nikunj Gurjar <39230292+Designicks@users.noreply.github.com>
2020-06-09 17:36:55 +0530

    Add files via upload

---
+++ build/precache-manifest.fcdcf965df438857b41a48379009f238.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "998a3dd400cf3cb73264dab721df4f90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa6176fa3e7fbd19e503",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6398462205d7df6b8685",
+    "url": "/static/css/main.2dd0ccbf.chunk.css"
+  },
+  {
+    "revision": "aa6176fa3e7fbd19e503",
+    "url": "/static/js/2.33f6d515.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.33f6d515.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6398462205d7df6b8685",
+    "url": "/static/js/main.e0cf770d.chunk.js"
+  },
+  {
+    "revision": "233dea908d4de0135036",
+    "url": "/static/js/runtime-main.c8273c4c.js"
+  }
+]);
